3381414ebcb154e43ff7ac54ad2cc62ab59b2ae0
I0039 <Prasad@spf-lap-026.local>
2020-01-04 14:36:51 +0530

    Initial Commit

---
+++ AssignmentGreedyGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AssignmentGreedyGame
+//
+//  Created by I0039 on 04/01/20.
+//  Copyright © 2020 I0039. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ AssignmentGreedyGameTests/AssignmentGreedyGameTests.swift
@@ -0,0 +1,34 @@
+//
+//  AssignmentGreedyGameTests.swift
+//  AssignmentGreedyGameTests
+//
+//  Created by I0039 on 04/01/20.
+//  Copyright © 2020 I0039. All rights reserved.
+//
+
+import XCTest
+@testable import AssignmentGreedyGame
+
+class AssignmentGreedyGameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
